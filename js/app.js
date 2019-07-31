00cd6eac4f518f474632ccc55f83016f37826563
shendo7 <rodneyjames@Rodneys-MacBook-Pro.local>
2019-07-31 08:19:06 -0700

    html_added

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Firebase
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBWGW-lgryhZBKs_v6UYRRc_-gDIEe0wUM",
+    authDomain: "train-time-93d15.firebaseapp.com",
+    databaseURL: "https://train-time-93d15.firebaseio.com",
+    projectId: "train-time-93d15",
+    storageBucket: "",
+    messagingSenderId: "884735784281",
+    appId: "1:884735784281:web:0f74aedf17b988a5"
+  };
+
+// Intialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
+
+
+
